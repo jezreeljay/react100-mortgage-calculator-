a1aae54c7ce2e9f0f0e58cd7b9fcec9d7104b460
jezreeljay <iam@jezreeljay.com>
2018-11-04 10:35:34 -0800

    Ads mortgage component and associated calculate functions. Passes test conditions.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,63 +6,97 @@ export default class App extends React.Component {
   constructor () {
     super();
     this.state = {
-      balanceInput: 0,
-      rateInput: 0,
-    }
+      balance: 0,
+      rate: 0,
+      term: 15,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.calculate = this.calculate.bind(this);
   }
 
-  userInput() {
-dg
-  }
+  handleChange(event) {
+    const target = event.target;
+    const value = parseFloat(target.value);
+    const name = target.name;
 
+    this.setState({
+      [name]: value
+    });
+
+  }
+    
+  calculate() {
+    const balance = this.state.balance;
+    const rate = (this.state.rate)/1200;
+    const term = (this.state.term*12);
+    const payment = balance*(rate * Math.pow((1 + rate), term))/(Math.pow((1 + rate), term) - 1);
+    document.getElementById("output").innerHTML = `$${payment.toFixed(2)} is your payment.`;
+  }
 
   render() {
+
     return (
       <div className='container'>
       <form className="horizontal-form clearfix">
       <div className="col-sm-offset-2 col-sm-10">
-        <h1>Mortgage Calculator</h1>
+        <h3>Mortgage Calculator</h3>
         <hr></hr>
       </div>
         <div className="form-group clearfix">
-          <label for="balance" className="col-sm-2 control-label">Loan Balance</label>
+          <label htmlFor="balance" className="col-sm-2 control-label">Loan Balance</label>
           <div className="col-sm-10">
             <input 
               className="form-control"
               type="number"
               name="balance"
-              value={this.state.balanceInput}
+              placeholder="0"
+              onChange={this.handleChange}
             />
           </div>
         </div>
         <div className="form-group clearfix">
-          <label for="rate" className="col-sm-2 control-label">Interest Rate (%)</label>
+          <label htmlFor="rate" className="col-sm-2 control-label">Interest Rate (%)</label>
           <div className="col-sm-10">
             <input 
               className="form-control"
               type="number"
               name="rate"
-              value={this.state.rateInput}
+              placeholder="0"
+              onChange={this.handleChange}
             />
           </div>
         </div>
         <div className="form-group clearfix">
-          <label for="term" className="col-sm-2 control-label">Loan Term (Years)</label>
+          <label htmlFor="term" className="col-sm-2 control-label">Loan Term (Years)</label>
           <div className="col-sm-10">
-            <select name="term" className="form-control">
-              <option>15</option>
-              <option>30</option>
+            <select name="term"
+            className="form-control"
+            value={this.state.name}
+            onChange= {this.handleChange}
+            >
+              <option value="15">15</option>
+              <option value="30">30</option>
             </select>
           </div>
         </div>
         <div className="form-group clearfix">
           <div className="col-sm-offset-2 col-sm-10">
-          <button trype="submit" className="btn btn-primary">Submit</button>
+          <button type="button"
+          name="submit"
+          className="btn btn-primary"
+          onClick={this.calculate}
+          >Submit</button>
           </div>
         </div>
       </form>
-        <div></div>
-        {/* your JSX goes here */}
+        <div className="form-group clearfix">
+          <div
+            id="output"
+            name="output"
+            className="col-sm-offset-2 col-sm-10"
+            >
+            </div>
+        </div>
       </div>
     );
   }
